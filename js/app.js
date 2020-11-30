8e57c2fc7cec1f5dc59bcf451a12a058f4230966
vaishnavme <vaishnavchandurkar21@outlook.com>
2020-11-30 14:51:11 +0530

    Feat: spinner added

--- js/app.js
+++ js/app.js
@@ -2,12 +2,27 @@ const quote = document.querySelector('#quote');
 const author = document.querySelector('#author');
 const button = document.querySelector('#btn');
 
+const loading = document.querySelector("#spinner");
+
 button.addEventListener('click', getQuote);
 
+function showSpinner() {
+    loading.className = "show";
+    setTimeout(() => {
+        loading.className = loading.className.replace("show", "");
+    }, 5000);
+}
+
+function hideSpinner() {
+    loading.className = loading.className.replace("show", "");
+}
+
 function getQuote() {
+    showSpinner()
      fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
      .then(res => res.json())
      .then(data => {
+         hideSpinner()
          quote.innerHTML = data[0].quote;
          author.innerHTML = data[0].character;
          document.getElementById('img').src = data[0].image;
